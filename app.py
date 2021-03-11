f143e1dc3a929ab3283fe23af4122660a1488c60
jvfoxiv <jvfoxiv@gmail.com>
2021-03-10 17:09:37 -0800

    Added Dockerfile and changes to app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return 'Hello World!'
+  return '<h1>Hello, Docker!</h1><img src="https://www.docker.com/sites/default/files/d8/2019-07/vertical-logo-monochromatic.png">'
 
 
 if __name__ == '__main__':
